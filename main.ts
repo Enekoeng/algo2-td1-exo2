bb37de3c0929605813e4559695afb9f2269a3b99
Enekoeng <68674472+Enekoeng@users.noreply.github.com>
2020-07-23 14:38:04 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showString("bonjour")
+basic.pause(500)
+basic.showString("10853")
+basic.pause(500)
+basic.showArrow(ArrowNames.South)
+basic.pause(500)
+basic.showIcon(IconNames.Ghost)
+basic.forever(function () {
+	
+})
